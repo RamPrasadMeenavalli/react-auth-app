feb110a59f907ab91aa5a89bff16311cd359644a
Ram <ram@spdeveloper.co.in>
2020-05-20 08:45:13 +0530

    Updated backend

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/hey', (req, res) => res.send('hello ' + req.headers.user + '!'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.get('/api/user', (req, res) => res.send({
+    user: req.headers.user,
+    logonUser:req.headers["logon-user"],
+    remoteUser:req.headers["remote-user"],
+    oidcUser:req.headers["oidc-user"],
+    oidcUserEmail:req.headers["oidc-user-email"],
+
+}))
 
 app.listen(8080)
--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-            Hello user!
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data:null,
+  }
+  render(){
+    const {data} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+              Hello
+              {data && data.user && (<span>{data.user}</span>)}
+          </p>
+        </header>
+      </div>
+    );
+  }
+
+  componentDidMount(){
+    fetch('/api/user').then(resp => resp.json().then(data => {
+      console.dir(data)
+    }))
+  }
 }
 
 export default App;

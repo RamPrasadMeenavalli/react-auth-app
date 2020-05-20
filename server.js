cc6c2829aefd639b973459765701ca30c56cdb74
Ram <ram@spdeveloper.co.in>
2020-05-20 09:12:56 +0530

    ..displaying username..

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const http = requrie('http');
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -7,7 +8,30 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.get('/api/user', (req, res) => res.send({
+app.get('/api/user', (req, res) => {
+    
+    const options = {
+        host: req.headers["oidc-userinfo-url"],
+        method: 'GET',
+        headers: req.headers
+    };
+
+    const oidcReq = http.request(options, (res) => {
+        console.dir(res);
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+            res.send(chunk);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+
+    oidcReq.end();
+   
+})
+
+res.send({
     user: req.headers.user,
     logonUser:req.headers["logon-user"],
     remoteUser:req.headers["remote-user"],

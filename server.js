b24d57eccd3e20b3db1c4ae0a67b0f37e20758a2
Ram <ram@spdeveloper.co.in>
2020-05-20 09:26:30 +0530

    ..displaying username..

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -8,36 +7,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.get('/api/user', (req, res) => {
-    
-    const options = {
-        host: req.headers["oidc-userinfo-url"],
-        method: 'GET',
-        headers: req.headers
-    };
-
-    const oidcReq = http.request(options, (res) => {
-        console.dir(res);
-        res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
-            res.send(chunk);
-        });
-        res.on('end', () => {
-            console.log('No more data in response.');
-        });
-    });
-
-    oidcReq.end();
-   
-})
-
-// res.send({
-//     user: req.headers.user,
-//     logonUser:req.headers["logon-user"],
-//     remoteUser:req.headers["remote-user"],
-//     oidcUser:req.headers["oidc-user"],
-//     oidcUserEmail:req.headers["oidc-user-email"],
-// }))
+app.get('/api/user', (req, res) => res.send({
+    user: req.headers.user,
+    logonUser:req.headers["logon-user"],
+    remoteUser:req.headers["remote-user"],
+    oidcUser:req.headers["oidc-user"],
+    oidcUserEmail:req.headers["oidc-user-email"],
+}))
 
 app.get('/api/userInfo', (req, res) => res.send(req.headers))
 
